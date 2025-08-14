f70bcf4a94cc3a0031fb11782c67ac107b1b9b2c
Mohammad Azhar <azharmohd2703@gmail.com>
2025-08-15 05:23:26 +0530

    added backend server to fronted

--- frontend/src/context/jobContext.jsx
+++ frontend/src/context/jobContext.jsx
@@ -8,19 +8,19 @@ export const JobProvider = ({ children }) => {
   const [state, dispatch] = useReducer(jobReducer, { jobs: [] });
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/jobs")
+    axios.get("https://job-tracker-79je.onrender.com/api/jobs")
       .then(res => dispatch({ type: "SET_JOBS", payload: res.data }))
       .catch(err => console.error(err));
   }, []);
 
   const addJob = (job) => {
-    axios.post("http://localhost:5000/api/jobs", job)
+    axios.post("https://job-tracker-79je.onrender.com/api/jobs", job)
       .then(res => dispatch({ type: "ADD_JOB", payload: res.data }))
       .catch(err => console.error(err));
   };
 
   const deleteJob = (id) => {
-    axios.delete(`http://localhost:5000/api/jobs/${id}`)
+    axios.delete(`https://job-tracker-79je.onrender.com/api/jobs/${id}`)
       .then(() => dispatch({ type: "DELETE_JOB", payload: id }))
       .catch(err => console.error(err));
   };
